b9da813ad573c352dfb6af9d5ec9854af6927244
tylerlantern <Nattapong@nathphngss-macbook-pro.local>
2016-09-12 23:15:17 +0700

    Initial Commit

---
+++ CyclingProgress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CyclingProgress
+//
+//  Created by Nattapong Unaregul on 12/09/2016.
+//  Copyright © 2016 Nattapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
